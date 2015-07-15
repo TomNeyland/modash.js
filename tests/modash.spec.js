b84ceccbb363816d02cf494604a0a08b17027b0d
Tom Neyland <tcneyland+github@gmail.com>
2015-07-15 00:46:41 -0400

    test(modash): Modash export expectations

---
+++ tests/modash.spec.js
@@ -0,0 +1,26 @@
+import Modash from '../src/modash';
+
+
+describe('Modash', function() {
+
+    it('should export aggregate as a function', function() {
+        expect(Modash.aggregate).to.be.a('function');
+    });
+
+    it('should export count as a function', function() {
+        expect(Modash.count).to.be.a('function');
+    });
+
+    it('should export distinct as a function', function() {
+        expect(Modash.distinct).to.be.a('function');
+    });
+
+    it('should export group as a function', function() {
+        expect(Modash.group).to.be.a('function');
+    });
+
+    it('should export mapReduce as a function', function() {
+        expect(Modash.mapReduce).to.be.a('function');
+    });
+
+});
