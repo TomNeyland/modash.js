379634765f7da383ecc9914b7977dc28dc735a61
Tom Neyland <tcneyland+github@gmail.com>
2015-07-17 01:52:51 -0400

    test(modash): Disable premature tests

--- tests/modash.spec.js
+++ tests/modash.spec.js
@@ -11,16 +11,16 @@ describe('Modash', function() {
         expect(Modash.count).to.be.a('function');
     });
 
-    it('should export distinct as a function', function() {
-        expect(Modash.distinct).to.be.a('function');
-    });
+    // it('should export distinct as a function', function() {
+    //     expect(Modash.distinct).to.be.a('function');
+    // });
 
-    it('should export group as a function', function() {
-        expect(Modash.group).to.be.a('function');
-    });
+    // it('should export group as a function', function() {
+    //     expect(Modash.group).to.be.a('function');
+    // });
 
-    it('should export mapReduce as a function', function() {
-        expect(Modash.mapReduce).to.be.a('function');
-    });
+    // it('should export mapReduce as a function', function() {
+    //     expect(Modash.mapReduce).to.be.a('function');
+    // });
 
 });
