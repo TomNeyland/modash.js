790b4cee2ed40929b883f9a96e81bcd85c79ce92
Tom Neyland <tcneyland+github@gmail.com>
2015-08-16 14:14:02 -0400

    test: Refactors tests and adds a new suite for the $group operator

--- tests/modash.spec.js
+++ tests/modash.spec.js
@@ -1,7 +1,7 @@
 import Modash from '../src/modash';
+import _ from 'lodash';
 
-
-describe('Modash', function() {
+describe('Modash Module Exports', function() {
 
     it('should export aggregate as a function', function() {
         expect(Modash.aggregate).to.be.a('function');
@@ -15,9 +15,18 @@ describe('Modash', function() {
     //     expect(Modash.distinct).to.be.a('function');
     // });
 
-    // it('should export group as a function', function() {
-    //     expect(Modash.group).to.be.a('function');
-    // });
+    it('should export $group as a function', function() {
+        expect(Modash.$group).to.be.a('function');
+    });
+
+    it('should export $project as a function', function() {
+        expect(Modash.$project).to.be.a('function');
+    });
+
+    it('should mix with lodash', function(){
+        _.mixin(Modash);
+        _(Modash).functions().map((func) => expect(_[func]).to.be.a('function')).commit();
+    });
 
     // it('should export mapReduce as a function', function() {
     //     expect(Modash.mapReduce).to.be.a('function');
