ed3a11e5904fa0678a51b9f9165f0cec7ab6edb0
Will Soto <will.soto9@gmail.com>
2015-08-19 18:42:23 -0400

    Update 2015-08-19T22:42:20.942Z

--- gulp/tasks/gh-pages.js
+++
@@ -1,9 +0,0 @@
-import config from '../config';
-
-import gulp from 'gulp';
-import pages from 'gulp-gh-pages';
-
-gulp.task('gh-pages', function() {
-    return gulp.src(`${config.docs}/**/*`)
-        .pipe(pages());
-});
